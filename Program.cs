b31c63adb95fcfd0c848b5ddb1adaf447b6ba436
Roman Pozdnyakov <proklus@gmail.com>
2022-12-23 23:58:50 +0300

    main processing block added

--- Program.cs
+++ Program.cs
@@ -2,14 +2,37 @@
 string? inputString = Console.ReadLine();
 string[] inputArray = inputString.Split(' ');
 
+int elementSize = 3;
+string[] outputArray = new string[0];
+int j = 0;
+
+for (int i = 0; i < inputArray.Length; i++)
+{
+    if (inputArray[i].Length <= elementSize)
+    {
+        Array.Resize(ref outputArray, outputArray.Length + 1);
+        outputArray[j++] = inputArray[i];
+    }
+}
+
 PrintStringArrayInSquares(inputArray);
+Console.Write(" -> ");
+PrintStringArrayInSquares(outputArray);
+Console.WriteLine();
 
 void PrintStringArrayInSquares(string[] array)
 {
-    Console.Write("[");
-    for (int i = 0; i < array.Length - 1; i++)
+    if (array.Length != 0)
     {
-        Console.Write($"\"{array[i]}\", ");
+        Console.Write("[");
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            Console.Write($"\"{array[i]}\", ");
+        }
+        Console.Write($"\"{array[array.Length - 1]}\"]");
     }
-    Console.WriteLine($"\"{array[array.Length - 1]}\"]");
+    else
+    {
+        Console.Write("[]");
+    }
 }
