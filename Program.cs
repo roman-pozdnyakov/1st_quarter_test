836238b3ce110c19665a9e478d1f55a18e34682f
Roman Pozdnyakov <proklus@gmail.com>
2022-12-23 23:24:16 +0300

    printout method added

--- Program.cs
+++ Program.cs
@@ -2,3 +2,14 @@
 string? inputString = Console.ReadLine();
 string[] inputArray = inputString.Split(' ');
 
+PrintStringArrayInSquares(inputArray);
+
+void PrintStringArrayInSquares(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"\"{array[i]}\", ");
+    }
+    Console.WriteLine($"\"{array[array.Length - 1]}\"]");
+}
